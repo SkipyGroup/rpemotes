51359267b83ec0bed6424bc0bbc2666ebf14f3f6
TotoLB <113319514+TotoLB@users.noreply.github.com>
2024-02-08 19:19:25 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,8 @@
 fx_version 'cerulean'
 game 'gta5'
 authors { 'TayMcKenzieNZ', 'MadsL', 'Mathu_lmn', 'Community' }
-description 'RPEmotes V1.3.8'
-version '1.3.8'
+description 'RPEmotes V1.3.9'
+version '1.3.9'
 lua54 'yes'
 
 dependencies {
